0b1f856ce057ff147791bca0957e83edab46411e
Sohum-Tripathi <68846631+Mcprince12@users.noreply.github.com>
2020-10-01 11:57:31 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Engine = Matter.Engine;
+const Constraint = Matter.Constraint;
+
+var stand;
+var block1, block2, block3, block4, block5, block6, block7, block8;
+var polygon;
+var engine, world;
+var slingshot;
+var pgImage;
+function preload(){
+  pgImg = loadImage("hexagon-removebg-preview.png");
+}
+function setup() {
+  createCanvas(800,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+  stand = new Ground(400, 400, 500, 100);
+
+  block1 = new Box(330, 235, 30, 40);
+  block2 = new Box(360, 235, 30, 40);
+  block3 = new Box(390, 235, 30, 40);
+  block4 = new Box(420, 235, 30, 40);
+  block5 = new Box(450, 235, 30, 40);
+
+  block6 = new Box(360, 195, 30, 40);
+  block7 = new Box(390, 195, 30, 40);
+  block8 = new Box(420, 195, 30, 40);
+
+  block9 = new Box(390, 100, 30, 40);
+
+  polygon = Bodies.circle(50, 200, 20);
+
+  
+  World.add(world, polygon);
+  slingshot = new SlingShot(polygon, {x:200, y:200});
+  
+}
+
+function draw() {
+  Engine.update(engine);
+  
+  background("black"); 
+
+    
+ 
+  
+  stand.display();
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+
+  block6.display();
+  block7.display();
+  block8.display();
+
+  block9.display();
+
+  slingshot.display();
+
+  imageMode(CENTER);
+  image(pgImg, polygon.position.x, polygon.position.y, 50, 50);
+  //ellipse(polygon.position.x, polygon.position.y, 20, 20);
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(polygon, {x: mouseX , y: mouseY});
+  
+}
+
+function mouseReleased(){
+  slingshot.fly();
+  
+}
